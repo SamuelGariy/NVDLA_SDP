c2b9c13df3bf03d58c0e592ba9201482e6b9b7a9
SamuelGariy <sgariy@princeton.edu>
2023-03-29 17:54:22 -0400

    17.54

--- build/sim_model/app/main.cc
+++ build/sim_model/app/main.cc
@@ -337,7 +337,7 @@ SC_MODULE(testbench) {
     wait(1000, SC_NS);
 
     // Log final outputs
-    fout << "Instruction number " << std::dec << instr_no << std::endl;
+ 
     fout << " NVDLA_SDP_S_PRODUCER => " << std::dec << (sc_dt::sc_bigint<16>)sdp_inst.sdp_s_producer << std::endl;
     fout << " NVDLA_SDP_D_OP_ENABLE => " << std::dec << (sc_dt::sc_bigint<16>)sdp_inst.sdp_group0_d_op_en << std::endl;
     fout << " SDP_D_STATUS => " << std::dec << (sc_dt::sc_bigint<16>)sdp_inst.sdp_group0_d_status_unequal << std::endl;
